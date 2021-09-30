10f0cde4a1e2b085ae40ef1d60d15f2be7a89714
John Doe <oleg.y07.10.03@gmail.com>
2021-09-30 20:45:08 +0300

    commited

--- 1.py
+++ 1.py
@@ -1,4 +1,14 @@
-A, B, C, D = int(input('Введите поочередно значения A, B, C, D: \n')), int(input()), int(input()), int(input())
+flag = True
+while flag:
+    try:
+        A = int(input('Введите поочередно числовые значения A, B, C, D: \n'))
+        B = int(input())
+        C = int(input())
+        D = int(input())
+    except ValueError:
+        print('Введите поочередно ЧИСЛОВЫЕ значения А, B, C, D, каждый с новой строки: ')
+    else:
+        flag = False
 maxAB = max(A, B)
 maxCD = max(C, D)
 minAB = min(A, B)
