5d5d1520ceba49afb28d01d95a95da7577d2820f
John Doe <oleg.y07.10.03@gmail.com>
2021-10-01 13:23:25 +0300

    commited

--- 1.py
+++ 1.py
@@ -1,19 +1,22 @@
 flag = True
 while flag:
     try:
-        A = int(input('Введите поочередно числовые значения A, B, C, D: \n'))
+        A = int(input('Введите поочередно положительные числовые значения A, B, C, D: \n'))
         B = int(input())
         C = int(input())
         D = int(input())
     except ValueError:
-        print('Введите поочередно ЧИСЛОВЫЕ значения А, B, C, D, каждый с новой строки: ')
+        print('Введите поочередно положительные ЧИСЛОВЫЕ значения А, B, C, D, каждый с новой строки: ')
     else:
         flag = False
-maxAB = max(A, B)
-maxCD = max(C, D)
-minAB = min(A, B)
-minCD = min(C, D)
-if (maxAB < maxCD) and (minAB < minCD):
-    print('Прямоугольник со сторонами AB может разместиться внутри прямоугольника CD')
-else:
-    print('Прямоугольник со сторонами AB НЕ может разместиться внутри прямоугольника CD')
+if (A <= 0) or (B <= 0) or (C <= 0) or (D <= 0):
+    print('Одно или все значения A B C D меньше или равно нулю. Ошибка.')
+if (A > 0) and (B > 0) or (C > 0) or (D > 0):
+    maxAB = max(A, B)
+    maxCD = max(C, D)
+    minAB = min(A, B)
+    minCD = min(C, D)
+    if (maxAB < maxCD) and (minAB < minCD):
+        print('Прямоугольник со сторонами AB может разместиться внутри прямоугольника CD')
+    else:
+        print('Прямоугольник со сторонами AB НЕ может разместиться внутри прямоугольника CD')
