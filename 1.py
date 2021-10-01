5793b443013c49af2a6ded3c082a72c5aaff56fa
John Doe <oleg.y07.10.03@gmail.com>
2021-10-01 14:11:23 +0300

    commited

--- 1.py
+++ 1.py
@@ -8,16 +8,15 @@ while flag:
     except ValueError:
         print('Введите поочередно ЦЕЛЫЕ ПОЛОЖИТЕЛЬНЫЕ ЧИСЛОВЫЕ значения А, B, C, D, каждый с новой строки: ')
     else:
-        if (A > 0) or (B > 0) or (C > 0) or (D > 0):
+        if (A > 0) and (B > 0) and (C > 0) and (D > 0):
             flag = False
         else:
             print('Одно или все значения A B C D меньше или равно нулю. Ошибка.')
-if (A > 0) and (B > 0) or (C > 0) or (D > 0):
-    maxAB = max(A, B)
-    maxCD = max(C, D)
-    minAB = min(A, B)
-    minCD = min(C, D)
-    if (maxAB < maxCD) and (minAB < minCD):
-        print('Прямоугольник со сторонами AB может разместиться внутри прямоугольника CD')
-    else:
-        print('Прямоугольник со сторонами AB НЕ может разместиться внутри прямоугольника CD')
+maxAB = max(A, B)
+maxCD = max(C, D)
+minAB = min(A, B)
+minCD = min(C, D)
+if (maxAB < maxCD) and (minAB < minCD):
+    print('Прямоугольник со сторонами AB может разместиться внутри прямоугольника CD')
+else:
+    print('Прямоугольник со сторонами AB НЕ может разместиться внутри прямоугольника CD')
